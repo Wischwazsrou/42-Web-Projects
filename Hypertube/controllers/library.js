8f1cb838ad44c2997c365a7144e99811c9755984
Felix Frimpong <frimpongfelix-jr@outlook.com>
2016-11-28 16:22:35 +0100

    Research by casting and by title ok. Starting to implement streaming part

--- Hypertube/controllers/library.js
+++ Hypertube/controllers/library.js
@@ -27,6 +27,7 @@ app.get("/", isLoggedIn, function (req, res) {
 
 app.get("/tvShow", isLoggedIn, function (req, res) {
     req.session.videoType = "tv";
+    req.session.search = "";
     request("https://api.themoviedb.org/3/discover/tv?api_key=08ec0cb8989aefee24d9b23a10574190",
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
@@ -82,23 +83,44 @@ app.get("/genre=:id", isLoggedIn, function (req, res) {
 
 app.get("/search", isLoggedIn, function (req, res) {
     if (req.session.search){
-        request("https://api.themoviedb.org/3/discover/" + req.session.videoType + "?api_key=08ec0cb8989aefee24d9b23a10574190" + req.session.search,
-            function (error, response, body) {
-                if (!error && response.statusCode == 200){
-                    var search = JSON.parse(body).results;
-                    request("https://api.themoviedb.org/3/genre/" + req.session.videoType + "/list?api_key=08ec0cb8989aefee24d9b23a10574190",
-                        function (error, response, body){
-                            if (!error && response.statusCode == 200){
-                                var genres = JSON.parse(body).genres;
-                                res.render("library", {movies: search, genres: genres});
-                            }
-                            else
-                                console.log(error);
-                        });
-                }
-                else
-                    console.log(error);
-            })
+        if (req.session.search.indexOf("&query=") == -1) {
+            request("https://api.themoviedb.org/3/discover/" + req.session.videoType + "?api_key=08ec0cb8989aefee24d9b23a10574190" + req.session.search,
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var search = JSON.parse(body).results;
+                        request("https://api.themoviedb.org/3/genre/" + req.session.videoType + "/list?api_key=08ec0cb8989aefee24d9b23a10574190",
+                            function (error, response, body) {
+                                if (!error && response.statusCode == 200) {
+                                    var genres = JSON.parse(body).genres;
+                                    res.render("library", {movies: search, genres: genres});
+                                }
+                                else
+                                    console.log(error);
+                            });
+                    }
+                    else
+                        console.log(error);
+                })
+        }
+        else{
+            request("https://api.themoviedb.org/3/search/" + req.session.videoType + "?api_key=08ec0cb8989aefee24d9b23a10574190" + req.session.search,
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var search = JSON.parse(body).results;
+                        request("https://api.themoviedb.org/3/genre/" + req.session.videoType + "/list?api_key=08ec0cb8989aefee24d9b23a10574190",
+                            function (error, response, body) {
+                                if (!error && response.statusCode == 200) {
+                                    var genres = JSON.parse(body).genres;
+                                    res.render("library", {movies: search, genres: genres});
+                                }
+                                else
+                                    console.log(error);
+                            });
+                    }
+                    else
+                        console.log(error);
+                })
+        }
     }
     else{
         req.flash("error", "Error:");
@@ -108,19 +130,56 @@ app.get("/search", isLoggedIn, function (req, res) {
 });
 
 app.post("/search", isLoggedIn, function (req, res) {
-    var genre   = "",
-        sort    = "",
-        year    = "";
+    var genre       = "",
+        sort        = "",
+        year        = "",
+        people      = "",
+        rate        = "";
 
-    if (req.body.genres !== "none")
-        genre = "&with_genres=" + req.body.genres;
-    if (req.body.sortBy !== "none")
-        sort = "&sort_by=" + req.body.sortBy;
-    if (req.body.years !== "none")
-        year = "&primary_release_year=" + req.body.years;
-
-    req.session.search = genre + sort + year;
-    res.redirect("/library/search");
+    if (!req.body.keywords) {
+        if (req.body.people) {
+            request("https://api.themoviedb.org/3/search/person?api_key=08ec0cb8989aefee24d9b23a10574190&query=" + req.body.people,
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200){
+                        people = "&with_people=" + JSON.parse(body).results[0].id;
+                        if (req.body.genres !== "none")
+                            genre = "&with_genres=" + req.body.genres;
+                        if (req.body.sortBy !== "none")
+                            sort = "&sort_by=" + req.body.sortBy;
+                        if (req.body.years !== "none") {
+                            if (req.session.videoType == "movie")
+                                year = "&primary_release_year=" + req.body.years;
+                            else
+                                year = "&first_air_date.gte=" + req.body.years;
+                        }
+                        if (req.body.rate != 0)
+                            rate = "&vote_average.gte=" + req.body.rate.toString();
+                        req.session.search = genre + sort + year + people + rate;
+                        res.redirect("/library/search");
+                    }
+            });
+        }
+        else{
+            if (req.body.genres !== "none")
+                genre = "&with_genres=" + req.body.genres;
+            if (req.body.sortBy !== "none")
+                sort = "&sort_by=" + req.body.sortBy;
+            if (req.body.years !== "none") {
+                if (req.session.videoType == "movie")
+                    year = "&primary_release_year=" + req.body.years;
+                else
+                    year = "&first_air_date.gte=" + req.body.years;
+            }
+            if (req.body.rate != 0)
+                rate = "&vote_average.gte=" + req.body.rate.toString();
+            req.session.search = genre + sort + year + rate;
+            res.redirect("/library/search");
+        }
+    }
+    else {
+        req.session.search = "&query=" + req.body.keywords;
+        res.redirect("/library/search");
+    }
 });
 
 function  isLoggedIn(req, res, next) {
--- Hypertube/controllers/streaming.js
+++ Hypertube/controllers/streaming.js
@@ -1,8 +1,19 @@
 var express     = require("express"),
-    app         = express();
+    app         = express(),
+    request     = require("request");
 
 app.get("/", isLoggedIn, function (req, res) {
     res.render("streaming");
+});
+
+app.get("/:id", isLoggedIn, function (req, res) {
+    request("https://api.themoviedb.org/3/movie/" +  req.params.id + "?api_key=08ec0cb8989aefee24d9b23a10574190&append_to_response=credits",
+        function (error, response, body) {
+            if (!error && response.statusCode == 200){
+                var movie = JSON.parse(body);
+                res.render("streaming", {movie: movie});
+            }
+        })
 });
 
 function  isLoggedIn(req, res, next) {
